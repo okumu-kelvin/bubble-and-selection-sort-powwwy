52894197cec96d478d36e8d5edc7481c7fa14ad4
powwwy <maxkimani12@gmail.com>
2025-06-20 09:02:55 +0300

    assignment

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,9 @@
-def bubble_sort(unsorted_list):
+def bubble_sort(arr):
     # TODO: Implement bubble sort
-    pass
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+pass
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,11 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
+    n = len(arr)
+    for i in range(n-1):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
     pass
